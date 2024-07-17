55ff3402cfa353f025d09b4b92cb211fb6c4cbcb
hackanonimous <esi.segurity@gmail.com>
2024-07-17 08:33:35 -0500

    add:ejericio uno
    se crea el ejercicio uno

---
+++ ejercicio_uno.py
@@ -0,0 +1,6 @@
+"""
+crear un funcion que reciba como parametro una lista de numeros y me retorne dos valores el numero menor y el numero mayor en un diccionario
+ejem:
+entrada: [4,7,10,4,1,0]
+salida : {menor:0,mayor:10}
+"""
