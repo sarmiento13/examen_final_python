eec97a7860493cc91ead0b3335927d4b7cf35a53
hackanonimous <esi.segurity@gmail.com>
2024-07-17 08:47:08 -0500

    add:ejericio cinco
    se crea el ejercicio cinco

---
+++ ejercicio_extra.py
@@ -0,0 +1,7 @@
+"""
+Crear un diccionario de 5 registros de tiendas comerciales
+y crear las siguientes funciones para el procesamiento de su informacion
+1. funcion que me permita editar el nombre de una las tiendas comerciales
+2. funcion que me permita actualizar el horario de atencion.
+3. funcion que me permita eliminar una tienda comercial
+"""
