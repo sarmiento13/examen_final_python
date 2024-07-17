e49a962e4347e6bc9c53923b2a1ded2e8c102521
sarmiento13 <barrientowsarmiento@gmail.com>
2024-07-17 11:05:39 -0500

    ejercicio_extra_media_hecha

--- ejercicio_extra.py
+++ ejercicio_extra.py
@@ -5,3 +5,28 @@ y crear las siguientes funciones para el procesamiento de su informacion
 2. funcion que me permita actualizar el horario de atencion.
 3. funcion que me permita eliminar una tienda comercial.
 """
+
+tiendas = {
+    'tienda1': {
+        'nombre': 'Tienda A',
+        'horario': '9:00 - 18:00'
+    },
+    'tienda2': {
+        'nombre': 'Tienda B',
+        'horario': '10:00 - 19:00'
+    },
+    'tienda3': {
+        'nombre': 'Tienda C',
+        'horario': '8:30 - 17:30'
+    },
+    'tienda4': {
+        'nombre': 'Tienda D',
+        'horario': '11:00 - 20:00'
+    },
+    'tienda5': {
+        'nombre': 'Tienda E',
+        'horario': '9:30 - 18:30'
+    }
+}
+
+
