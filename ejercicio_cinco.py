b4da7bbde9bb01960a3192db4f51e9fa88b710a9
hackanonimous <esi.segurity@gmail.com>
2024-07-17 08:43:49 -0500

    add:ejericio cinco
    se crea el ejercicio cinco

---
+++ ejercicio_cinco.py
@@ -0,0 +1,6 @@
+"""
+Crear una funcion que me me retorne un diccionario con los datos personales de un alumno
+ejm:
+entrada: ("jose","alvarez","30","APSTI","III")
+salida: {nombre:"jose",apellido:"alvarez",edad:"30",programa_estudio:"APSTI",semestre:"III"}
+"""
