2738cb734d7a8c7be2cf942ae0fd6bbf6b69071c
hackanonimous <esi.segurity@gmail.com>
2024-07-17 08:39:44 -0500

    add:ejericio tres
    se crea el ejercicio tres

---
+++ ejercicio_tres.py
@@ -0,0 +1,6 @@
+"""
+crear un funcion que me retorne un diccioonario de la cantidad de vocales que aparecen en un texto pasado por parametro el diccionario debera ser credo por comprension de diccionarios
+ejm:
+entrada: eucalipto
+salida: {e:1,u:1,a:1,i:1,o:1}
+"""
