9ff9fb99c066f709b5b8abaa7bbef785b46d10a3
sarmiento13 <barrientowsarmiento@gmail.com>
2024-07-17 10:37:34 -0500

    ejercicio_uno_resuelto

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,14 +4,26 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
-lista=[4,7,10,4,1,0]
-def encontrar_min_y_max(lista):
-    if not lista:
-        return{}
-    minimo = min(lista)
-    maximo = max (lista)
-    return{"minimo":minimo,"maximo":maximo}
-#resultado = encontrar_min_y_max(lista)
-#print(resultado)
 
-print(encontrar_min_y_max([5,2,7,8,9,5]))
+def encontrar_menor_y_mayor(lista):
+    if not lista: 
+        return {}  
+    menor = lista[0]  
+    mayor = lista[0]  
+    
+    for numero in lista:
+        if numero < menor:
+            menor = numero
+        if numero > mayor:
+            mayor = numero
+    
+    resultado = {
+        'menor': menor,
+        'mayor': mayor
+    }
+    
+    return resultado
+
+lista_numeros = [4, 7, 8, 2, 10, 4, 1, 0]
+resultado = encontrar_menor_y_mayor(lista_numeros)
+print(resultado)
